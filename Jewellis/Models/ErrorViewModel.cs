028c89c0b34bc8e17b8a09afbf807e0d6f232d4a
Ohad Shai <ohad12131@gmail.com>
2021-04-28 12:14:51 +0300

    Add project files.

---
+++ Jewellis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jewellis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

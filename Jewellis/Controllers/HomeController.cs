1ed2f65c6dc3b7f15a64012ab68913b83b60e822
Ohad Shai <officialohadshai@gmail.com>
2021-05-09 11:10:24 +0300

    [Added] About page.

--- Jewellis/Controllers/HomeController.cs
+++ Jewellis/Controllers/HomeController.cs
@@ -1,24 +1,18 @@
 ﻿using Jewellis.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Jewellis.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
@@ -33,5 +27,6 @@ namespace Jewellis.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }

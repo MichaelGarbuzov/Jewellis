176f667b3b6a0027151236cb1217e0357f7892e9
Ohad Shai <officialohadshai@gmail.com>
2021-05-15 21:40:21 +0300

    [Added] Design for account pages like: orders, wishlist, and profile info.

--- Jewellis/Controllers/AccountController.cs
+++ Jewellis/Controllers/AccountController.cs
@@ -6,20 +6,44 @@ namespace Jewellis.Controllers
     public class AccountController : Controller
     {
 
-        [Route("/login")]
-        public IActionResult Login()
-        {
-            return View();
-        }
-
         [Route("/register")]
         public IActionResult Register()
         {
             return View();
         }
 
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        [Route("orders")]
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
         [Route("wishlist")]
         public IActionResult Wishlist()
+        {
+            return View();
+        }
+
+        [Route("profile")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [Route("address")]
+        public IActionResult Address()
         {
             return View();
         }

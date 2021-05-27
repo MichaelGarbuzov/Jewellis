31e2ad3a4d543d3bf36d489324a0efb547e1d428
michael Garbuzov <michaelgarbuzov1994@gmail.com>
2021-05-27 13:05:36 +0300

    Added branch model object, and updated stores page model. Also added team photo in about page and section.

--- Jewellis/Controllers/HomeController.cs
+++ Jewellis/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jewellis.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace Jewellis.Controllers
 {
@@ -45,7 +48,41 @@ namespace Jewellis.Controllers
         [Route("/stores")]
         public IActionResult Stores()
         {
-            return View();
+            List<Branch> branches = new List<Branch>();
+            branches.Add(new Branch()
+            {
+                Id = 1,
+                Name = "Tel Aviv - Dizengoff Center",
+                Adrress = "Dizengoff St. 50, Tel Aviv-Yafo",
+                PhoneNumber = "03-1234567",
+                OpeningHours = "Sunday-Thursday: 10:00-20:00\r\nFriday and Holidays: 10:00-14:00\r\nSaturday: Closed",
+                LocationAltitude = 32.0748402,
+                LocationLongitude = 34.7751358,
+                DateLastModified = DateTime.Now
+            });
+            branches.Add(new Branch()
+            {
+                Id = 2,
+                Name = "Tel Aviv - Shenkin Street",
+                Adrress = "Shenkin St. 12, Tel Aviv-Yafo",
+                PhoneNumber = "03-1234567",
+                OpeningHours = "Sunday-Thursday: 10:00-20:00\r\nFriday and Holidays: 10:00-14:00\r\nSaturday: Closed",
+                LocationAltitude = 32.0687168,
+                LocationLongitude = 34.7780078,
+                DateLastModified = DateTime.Now
+            });
+            branches.Add(new Branch()
+            {
+                Id = 3,
+                Name = "Rishon Lezion - HaZahav Mall",
+                Adrress = "Herzl 21, Rishon Lezion",
+                PhoneNumber = "03-1234567",
+                OpeningHours = "Sunday-Thursday: 10:00-20:00\r\nFriday and Holidays: 10:00-14:00\r\nSaturday: Closed",
+                LocationAltitude = 31.9913925,
+                LocationLongitude = 34.7764307,
+                DateLastModified = DateTime.Now
+            });
+            return View(branches);
         }
 
     }
---
+++ Jewellis/Models/Branch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jewellis.Models
+{
+    public class Branch
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Adrress { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string OpeningHours { get; set; }
+
+        public double LocationAltitude { get; set; }
+
+        public double LocationLongitude { get; set; }
+
+        public DateTime DateLastModified { get; set; }
+
+    }
+}
